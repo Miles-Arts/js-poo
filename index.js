a14a5812f83325a4ee64434692525ccebfb1247f
Milton Figueredo Miles <gratno@gmail.com>
2022-10-27 12:59:28 -0500

    JS POO

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+    nombreCliente;
+    cedulacliente;
+    numeroCuenta;
+    saldoCuenta;
+}
+
+const nombreCliente = "Anita";
+const cedulacliente = "123456";
+const numeroCuenta = "09876";
+const saldoCuenta = "1000";
+
+
