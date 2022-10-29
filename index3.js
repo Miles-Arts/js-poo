5efde5e45845c9f8309554b5b59a9d1d3ceafe70
Milton Figueredo Miles <gratno@gmail.com>
2022-10-29 11:29:29 -0500

    Datos devolver saldo return

--- index3.js
+++ index3.js
@@ -19,21 +19,38 @@ class CuentaCorriente {
     depositoEnCuenta(valor) {
         if (valor > 0)
             this.#saldo += valor;
+        return this.#saldo;
     }
 
     retirarDeCuenta(valor) {
         //this.saldo = this.saldo - valor;
         if (valor <= this.#saldo) // if para no generar retiro cuando hay menos saldo
             this.#saldo -= valor;
+        return this.#saldo;
     }
-}
 
+    verSaldo() {
+        return this.#saldo;
+    }
+
+}
 cuentaDeLeonardo = new CuentaCorriente();
+
+let saldo = cuentaDeLeonardo.verSaldo();
+console.log(`El saldo actual es: ${saldo}`);
+
+
 //cuentaDeLeonardo.#saldo = 10;
 
 cuentaDeLeonardo.depositoEnCuenta(100);
-console.log(cuentaDeLeonardo);
+saldo = cuentaDeLeonardo.verSaldo();
+console.log(`El saldo actual es: ${saldo}`);
+//console.log(cuentaDeLeonardo);
 cuentaDeLeonardo.retirarDeCuenta(100);
-console.log(cuentaDeLeonardo);
-/*cuentaDeLeonardo.depositoEnCuenta(-100);
-console.log(cuentaDeLeonardo);*/
+saldo = cuentaDeLeonardo.verSaldo();
+console.log(`El saldo actual es: ${saldo}`);
+//console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.depositoEnCuenta(10);
+saldo = cuentaDeLeonardo.verSaldo();
+console.log(`El saldo actual es: ${saldo}`);
+//console.log(cuentaDeLeonardo);
