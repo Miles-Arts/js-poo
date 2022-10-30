6a9294b05cfe2f100933f5f607880cfbfbcbcd3f
Milton Figueredo Miles <gratno@gmail.com>
2022-10-30 14:34:26 -0500

    Contructor en cliente

--- cliente.js
+++ cliente.js
@@ -1,11 +1,12 @@
 //BANCO POPULAR //Definición de clases
 
 export class Cliente {
+
     nombreCliente;
     cedulaCliente;
     rutCliente;
 
-    contructor(nombreCliente, cedulaCliente, rutCliente) {
+   constructor(nombreCliente, cedulaCliente, rutCliente) {
         this.nombreCliente = nombreCliente;
         this.cedulaCliente = cedulaCliente;
         this.rutCliente = rutCliente;
--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 //BANCO POPULAR //Definición de clases
-
 import { Cliente } from "./cliente.js";
 import { CuentaCorriente } from "./cuentaCorriente.js";
 
-const cliente = new Cliente('Leonardo','13804050','123224');
-const cliente2 = new Cliente('María','16979808','8989');
-
-console.log(cliente);
-console.log(cliente2);
+const cliente = new Cliente("Leonardo", "13804050", "123224");
+const cliente2 = new Cliente("María", "16979808", "8989");
 
 const cuentaDeLeonardo = new CuentaCorriente(cliente, "1", "001");
-
-//let saldo = cuentaDeLeonardo.verSaldo();
-
-//console.log(cliente);
-//saldo = cuentaDeLeonardo.depositoEnCuenta(150);
-
 const cuentaDeMaria = new CuentaCorriente(cliente2, "2", "002");
 
+let saldo = cuentaDeLeonardo.verSaldo();
 
-//cuentaDeLeonardo.transferirParaCuenta(100, cuentaDeMaria);
-//console.log(cuentaDeMaria.cliente);
+saldo = cuentaDeLeonardo.depositoEnCuenta(150);
+console.log("El saldo actual (CuentaLeonardo)" + saldo);
 
-//cuentaDeMaria.cliente = 0;
-//console.log(cuentaDeMaria.cliente);
-
-/*
-const saldoMaria = cuentaDeMaria.verSaldo();
-console.log(`El saldo actual (cuentaMaria) ${saldoMaria}`);
-
-const saldoLeonardo = cuentaDeLeonardo.verSaldo();
-console.log(`El saldo actual (cuentaLeonardo) ${saldoLeonardo}`);
-*/
+console.log(cuentaDeLeonardo.cliente);
+console.log(cuentaDeMaria.cliente);
