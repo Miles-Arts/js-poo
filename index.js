cfcf861cf0f7ec22e5c756dc48fba441a382bf54
Milton Figueredo Miles <gratno@gmail.com>
2022-11-02 12:54:04 -0500

    Cuenta de Ahorros -nuevo cliente-

--- index.js
+++ index.js
@@ -9,15 +9,7 @@ const cliente2 = new Cliente("María", "16979808", "8989");
 const cuentaDeLeonardo = new CuentaCorriente(cliente, "1", "001");
 const cuentaDeMaria = new CuentaCorriente(cliente2, "2", "002");
 
-let saldo = cuentaDeLeonardo.verSaldo();
+const cuentaAhorroLeonardo = new CuentaAhorro(cliente, "13804050", "001", 0);
 
-saldo = cuentaDeLeonardo.depositoEnCuenta(150);
-
-console.log(CuentaCorriente.cantidadCuentas);
-
-new CuentaCorriente(cliente2, "2", "002");
-new CuentaCorriente(cliente2, "2", "002");
-new CuentaCorriente(cliente2, "2", "002");
-new CuentaCorriente(cliente2, "2", "002");
-
-console.log(CuentaCorriente.cantidadCuentas);
+console.log(cuentaDeLeonardo);
+console.log(cuentaAhorroLeonardo);
