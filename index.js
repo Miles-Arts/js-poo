da0de784dbc07f7728bb51b92b1772d7893cb24f
Milton Figueredo Miles <gratno@gmail.com>
2022-11-02 13:11:15 -0500

    Eliminando código repetido

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ import { Cliente } from "./cliente.js";
 const cliente = new Cliente("Leonardo", "13804050", "123224");
 const cliente2 = new Cliente("María", "16979808", "8989");
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, "1", "001");
-const cuentaDeMaria = new CuentaCorriente(cliente2, "2", "002");
+const cuentaDeLeonardo = new Cuenta(cliente, "1", "001", 0);
+const cuentaDeMaria = new Cuenta(cliente2, "2", "002", 0);
 
-const cuentaAhorroLeonardo = new CuentaAhorro(cliente, "13804050", "001", 0);
+const cuentaAhorroLeonardo = new Cuenta(cliente, "13804050", "001", 0);
 
 console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.depositoEnCuenta(150);
+console.log(cuentaDeLeonardo.verSaldo());
+cuentaDeLeonardo.retirarDeCuenta(70);
+console.log(cuentaDeLeonardo.verSaldo());
+console.log("-----------");
 console.log(cuentaAhorroLeonardo);
+console.log(cuentaAhorroLeonardo.verSaldo());
+cuentaAhorroLeonardo.depositoEnCuenta(200);
+console.log(cuentaAhorroLeonardo.verSaldo());
+cuentaAhorroLeonardo.retirarDeCuenta(120);
+console.log(cuentaAhorroLeonardo.verSaldo());
