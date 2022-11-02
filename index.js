ad8448f8eb2c5a70f612e8a1d1116d19d7b72aad
Milton Figueredo Miles <gratno@gmail.com>
2022-11-02 13:53:11 -0500

    Herencia

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ import { Cliente } from "./cliente.js";
 const cliente = new Cliente("Leonardo", "13804050", "123224");
 const cliente2 = new Cliente("María", "16979808", "8989");
 
-const cuentaDeLeonardo = new Cuenta(cliente, "1", "001", 0);
-const cuentaDeMaria = new Cuenta(cliente2, "2", "002", 0);
+const cuentaDeLeonardo = new Cuenta("Corriente", cliente, "1", "001", 0);
+const cuentaDeMaria = new Cuenta("Corriente", cliente2, "2", "002", 0);
 
-const cuentaAhorroLeonardo = new Cuenta(cliente, "13804050", "001", 0);
+const cuentaAhorroLeonardo = new Cuenta("Ahorro" ,cliente, "13804050", "001", 0);
 
 console.log(cuentaDeLeonardo);
 cuentaDeLeonardo.depositoEnCuenta(150);
 console.log(cuentaDeLeonardo.verSaldo());
 cuentaDeLeonardo.retirarDeCuenta(70);
 console.log(cuentaDeLeonardo.verSaldo());
+
 console.log("-----------");
+
 console.log(cuentaAhorroLeonardo);
 console.log(cuentaAhorroLeonardo.verSaldo());
 cuentaAhorroLeonardo.depositoEnCuenta(200);
