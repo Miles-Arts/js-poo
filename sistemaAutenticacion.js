00e5e49bdb17b3d3b1f64d2aeb75c63f5b5cd696
Milton Figueredo Miles <gratno@gmail.com>
2022-11-06 12:13:12 -0500

    propiedades tipo

--- sistemaAutenticacion.js
+++ sistemaAutenticacion.js
@@ -3,10 +3,10 @@ export class SistemaAutenticacion {
 
     static login(usuario, clave) {
 
-        if ("autenticable" in usuario) {
+        if ("autenticable" in usuario && usuario.autenticable instanceof Function) {
             return usuario.autenticable(clave);
         } else
             return false;
-       
+
     }
 }
